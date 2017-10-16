24c349076d0a601e0f13b79eab5e260872bf59f5
Abel <canet.ach@gmail.com>
2017-10-16 17:55:38 +0200

    añadido componente search

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import SearchForm from './SearchForm'
 
-const Home = () => (<h1>Estoy en la HOME</h1>)
+const Home = () => (
+	<div>
+	<h1>Estoy en la HOME</h1>
+	<SearchForm />
+</div>
+	)
 
 export default Home;
