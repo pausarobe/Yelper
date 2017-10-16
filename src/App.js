4af4a319219b05d012cac89fde83c7f3b0eaaf1a
Abel <canet.ach@gmail.com>
2017-10-16 22:44:46 +0200

    Añadido el state con el resultado de la api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import {getTestData } from './services/Api.js'
 import Main from './components/Main'
 import { BrowserRouter } from 'react-router-dom'
 
 class App extends Component {
   render() {
-  	const callApi= getTestData()
-    return ( 
+  	return ( 
     	<BrowserRouter>
     		<Main />
     	</BrowserRouter>
--- src/components/ShowSearchResults.js
+++ src/components/ShowSearchResults.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import Results from './Results'
-import {getTestData } from '../services/Api.js'
+import {getYelpData } from '../services/Api.js'
 
 class  ShowSearchResults extends Component {
 	constructor(){
 		super()
 		this.state={
-
-		}
+	      	result: [{
+		        id: '',
+		        name: '',
+		        image_url: '',
+		        is_closed: '',
+		        rating: ''        
+      		}]
+    	}
+    	//this.getApiData=this.getApiData.bind(this)
 	}
+	getApiData(){
+    getYelpData(this.props.match.params.query).then(
+      dataSearchYelp => {
+        this.setState({
+          result:[...dataSearchYelp]
+            .map(function(yelpData){
+              return (   {
+                  id: yelpData.id,
+                  name: yelpData.name,
+                  image_url: yelpData.image_url,
+                  is_closed: yelpData.is_closed,
+                  rating: yelpData.rating
+              })
+          })
+        })
+      })
+  	}
+
+  	componentDidMount(){
+    this.getApiData(this.props.match.params.query)   
+  }
 	render(){
+		console.log(this.state)
 		return (
 			<div>
 				<h1>Estoy en la ShowSearchResults</h1> 
