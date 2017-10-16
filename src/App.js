d9e4e1e7daceff458f763aef64a19f3cb8d32689
Nacho Torrella <nachotorrella@gmail.com>
2017-10-16 17:27:17 +0200

    hello people

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return ( <h1>Esto es yelper </h1>)
+  componentDidMount () {
+      axios.get('https://api.yelp .com/v3/businesses/search?term=Sushi&location=Boston', {
+          headers: {
+              Authorization: `Bearer RmHQV-aiyyC-8L0UnKViDsgcCxXiTpIFvV2tfqO5TM4l6poX81umJNu8pctONiOpdMD1JAC27_LD0NeNNfS1_q_nXe9es8bAYol8MXOL_Up4OAMDc8vfmCo6rXPkWXYx`
+          }
+      })
+      .then(function (res) {
+          console.log(res)
+      })
+      .catch(function (err) {
+          console.log(err)
+      })
+  }
+
+  render () {
+    return (<h1>Esto es yelper </h1>)
   }
 }
 
-export default App;
+export default App
