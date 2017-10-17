13e1a93a3ee8610ea646ef336f64bffb1efc6b5a
kevinrosmusic <kevinros1992@hotmail.com>
2017-10-17 11:58:34 +0100

    Kevin y Abel

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,14 +4,15 @@ import './Results.css'
 class Results extends Component {
 
 	render() {
-        console.log(this.props.inputresults)
-    	return (<div className="container">
-    <div className="row">
-    <div className="well">
-        <h1 className="text-center">Results Page</h1>
-        <div className="list-group">
+        console.log("results",this.props.inputresults)
+    	return (
+            <div className="container">
+                <div className="row">
+                    <div className="well">
+                        <h1 className="text-center">Results Page</h1>
+                        <div className="list-group">
         {
-            this.props.inputresults.result.map((res)=> {
+            this.props.inputresults.map((res)=> {
                 return (<a href={res.url} className="list-group-item">
                                 <div className="media col-md-3">
                                     <figure className="pull-left">
@@ -41,11 +42,12 @@ class Results extends Component {
             })
               
         }
+                         </div>
+                    </div>
+                </div>
         </div>
-        </div>
-  </div>
-</div>)
-}
+        )
+    }
 }
 
 export default Results;
