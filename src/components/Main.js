06eb9352537f1a96e9d001b8ab332ebdce09682f
Nacho Torrella <nachotorrella@gmail.com>
2017-10-17 13:02:57 +0200

    nacho

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+
 import Home from './Home'
 import ShowSearchResults from './ShowSearchResults'
-
+import ShowSpecificResults from './ShowSpecificResults'
 
 const Main = (props) => {
 	return (
-	<div>
-		<Switch>
-	      <Route exact path='/' component={Home}/>
-	      <Route path='/search/:query' component={ShowSearchResults}/>
-	    </Switch>
+  	<div>
+  		<Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/search/:query' component={ShowSearchResults} />
+        <Route path='/specific/:query' component={ShowSpecificResults} />
+      </Switch>
   	</div>
-	)}
+  )
+}
 
-export default Main;
+export default Main
---
+++ src/components/ShowSpecficResults.js
@@ -0,0 +1,1 @@
+ShowSpecficResults.js
